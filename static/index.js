1f643748fcab6ba01990d7e83fe3534c4e2a6938
tzcTeddy <1342671790@qq.com>
2019-08-24 22:14:13 +0800

    修改

--- static/index.js
+++ static/index.js
@@ -1,17 +1,5 @@
 window.onload=function(){
-    setTimeout(function(){
-        var str=`<li>
-                <img src="/static/logo.png" data-origin="static/logo.png" alt="对点">
-            </li>
-            <li>
-                <img src="/static/logo_text.png" data-origin="static/logo_text.png" alt="对点儿">
-            </li>`;
-    var navNode=document.getElementsByClassName('app-nav')[0];
-    console.log(navNode)
-    var targetUl=navNode.getElementsByTagName('ul')[0];
-       targetUl.innerHTML=str+targetUl.innerHTML;
-  
-    },0)
+
 }
 
 // * ![对点](static/logo.png)
