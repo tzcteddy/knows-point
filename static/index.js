e334bc63133f0365eadce0c0fab4658bcc0da74a
tzcteddy <1342671790@qq.com>
2025-12-16 09:12:07 +0800

    feat: add

--- static/index.js
+++ static/index.js
@@ -43,23 +43,16 @@ window.onload=function(){
     }
   })
   window.onpopstate=function(){
-      if (document.readyState === "complete") {
-      setTimeout(()=>{
-        var script=document.getElementById('built-in');
-        if(script){
-          eval(console.log('内置页面js执行'))
-          eval(script.innerText)
-        }
-      },0)
-    }
+    //   if (document.readyState === "complete") {
+    //   setTimeout(()=>{
+    //     var script=document.getElementById('built-in');
+    //     if(script){
+    //       eval(console.log('内置页面js执行'))
+    //       eval(script.innerText)
+    //     }
+    //   },0)
+    // }
   }
-  setTimeout(()=>{
-        var script=document.getElementById('built-in');
-        if(script){
-          eval(console.log('内置页面js执行'))
-          eval(script.innerText)
-        }
-      },0)
 }
 
 // * ![对点](static/logo.png)
